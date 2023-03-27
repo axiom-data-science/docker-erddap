8b5f1a1547e9df9c6f65c4067754f3c84980a9bd
Shane St Savage <shane@axds.co>
2023-03-27 08:52:01 -0700

    datasetsd: add -r flag to refresh datasets missing from running config via flags

--- datasets.d.sh
+++ datasets.d.sh
@@ -11,7 +11,8 @@ DATASETSD_DIR="${DATASETSD_DIR:-/datasets.d}"
 DATASETSD_OUTPUT_PATH="${DATASETSD_OUTPUT_PATH:-/usr/local/tomcat/content/erddap/datasets.xml}"
 DATASETSD_MARK_REMOVED_DATASETS_INACTIVE="${DATASETSD_MARK_REMOVED_DATASETS_INACTIVE:-0}"
 DATASETSD_WRITE_TO_OUTPUT_PATH="${DATASETSD_WRITE_TO_OUTPUT_PATH:-0}"
-while getopts ":d:io:w" opt; do
+DATASETSD_REFRESH_MISSING_DATASETS="${DATASETSD_REFRESH_MISSING_DATASETS:-0}"
+while getopts ":d:io:rw" opt; do
   case ${opt} in
     d )
       DATASETSD_DIR="$OPTARG"
@@ -21,6 +22,9 @@ while getopts ":d:io:w" opt; do
       ;;
     o )
       DATASETSD_OUTPUT_PATH="$OPTARG"
+      ;;
+    r )
+      DATASETSD_REFRESH_MISSING_DATASETS=1
       ;;
     w )
       DATASETSD_WRITE_TO_OUTPUT_PATH=1
@@ -57,9 +61,7 @@ if [ "$DATASETSD_MARK_REMOVED_DATASETS_INACTIVE" == "1" ] && [ -n "$DATASETSD_OU
   #if any datasets there not marked inactive are missing from our generated file
   #add them as inactive datasets so ERDDAP can cleanly make them inactive
   #https://coastwatch.pfeg.noaa.gov/erddap/download/setupDatasetsXml.html#active
-  echo "Detecting removed datasets"
   while read -r inactive_dataset; do
-    echo "Detected removed dataset ${inactive_dataset}"
     DXML=$(echo "$DXML" | xmlstarlet edit -P -s /erddapDatasets --type elem --name inactiveDataset -v "" \
       -i //inactiveDataset -t attr -n "datasetID" -v "${inactive_dataset}" \
       -i //inactiveDataset -t attr -n "active" -v "false" \
@@ -75,6 +77,13 @@ DXML=$(echo "$DXML" | xmlstarlet edit --inplace)
 #write output to target file if one was provided and write to stdout flag was not provided
 if [ -n "$DATASETSD_OUTPUT_PATH" ] && [ "$DATASETSD_WRITE_TO_OUTPUT_PATH" == "1" ]; then
   echo "$DXML" > "$DATASETSD_OUTPUT_PATH"
+  #set refresh flags for any datasetIDs in datasets.xml that are not in the running ERDDAP config if the refresh option was set
+  if [ -n "$DATASETSD_REFRESH_MISSING_DATASETS" ] && [ "$DATASETSD_REFRESH_MISSING_DATASETS" == "1" ]; then
+    comm -23 \
+      <(xmlstarlet select -t -v "/erddapDatasets/dataset/@datasetID" "$DATASETSD_OUTPUT_PATH" | sort) \
+      <(curl -sS "http://localhost:8080/erddap/tabledap/allDatasets.csv0?datasetID" | grep -v "^allDatasets$" | sort) \
+      | xargs -I{} touch /erddapData/flag/{}
+  fi
 else
   echo "$DXML"
 fi
